e60ca1af053916697afa2b5452346d6eb87c00bb
Sona Sreedhar <sonasreedhar44@gmail.com>
2025-01-06 11:33:53 +0530

    reverse string

---
+++ index6.js
@@ -0,0 +1,16 @@
+// Example 1:
+
+// Input: s = ["h","e","l","l","o"]
+// Output: ["o","l","l","e","h"]
+// Example 2:
+
+// Input: s = ["H","a","n","n","a","h"]
+// Output: ["h","a","n","n","a","H"]
+
+
+function reverse(input){
+   const b = input.reverse()
+   return b
+}
+var input = ["h","e","l","l","o"]
+console.log(reverse(input))
