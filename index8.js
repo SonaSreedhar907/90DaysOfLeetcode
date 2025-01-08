c651989ba7484b82aeb66328a27233a58480a3da
Sona Sreedhar <sonasreedhar44@gmail.com>
2025-01-08 11:24:59 +0530

    valid palindrome

--- index8.js
+++ index8.js
@@ -1,27 +1,40 @@
+//valid palindrome
+
 // Example 1:
 
-// Input: s = "Hello"
-// Output: "hello"
+// Input: s = "A man, a plan, a canal: Panama"
+// Output: true
+// Explanation: "amanaplanacanalpanama" is a palindrome.
 // Example 2:
 
-// Input: s = "here"
-// Output: "here"
+// Input: s = "race a car"
+// Output: false
+// Explanation: "raceacar" is not a palindrome.
 // Example 3:
 
-// Input: s = "LOVELY"
-// Output: "lovely"
+// Input: s = " "
+// Output: true
+// Explanation: s is an empty string "" after removing non-alphanumeric characters.
+// Since an empty string reads the same forward and backward, it is a palindrome.
 
 
-function toUpperCase (s){
-  let b=""
-  for(let i=0;i<s.length;i++){
-    if(i == 0){
-        b += s[i].toUpperCase()
-    }else{
-        b += s[i]
-    }
-  }
-  return b
+function palindrome (s){
+ let b=s.replace(/[^A-Za-z0-9]/g,'').toLowerCase()
+ let c = b.split('').reverse().join('')
+ if(b == c){
+    return true
+ }
+ return false
 }
-var s = "here"
-console.log(toUpperCase(s))
+var s = "race a car "
+console.log(palindrome(s))
+
+
+// the reverse() method can be used to reverse a string, but
+//  it requires converting
+//  the string into an array first.
+
+//split('') used to convert the string into an array of characters(string -> array)
+// reverse() used to reverse the elements in the array
+//join('') combine the elements of the array back
+           //into string   (array ->  string)
