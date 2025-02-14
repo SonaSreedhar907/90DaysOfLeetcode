39da91697ec925a2c336dae88cef41b965e7a9fc
Sona Sreedhar <sonasreedhar44@gmail.com>
2025-02-14 16:56:20 +0530

    Reverse words in a string

---
+++ leetcode/index45.js
@@ -0,0 +1,27 @@
+//151. Reverse Words in a String
+
+// Example 1:
+
+// Input: s = "the sky is blue"
+// Output: "blue is sky the"
+// Example 2:
+
+// Input: s = "  hello world  "
+// Output: "world hello"
+// Explanation: Your reversed string should not contain leading or trailing spaces.
+// Example 3:
+
+// Input: s = "a good   example"
+// Output: "example good a"
+// Explanation: You need to reduce multiple spaces between two words to a single space in the reversed string.
+
+//note - .split(/\s+/) ensures that extra spaces are ignored
+
+function reverseWords(s){
+  var b = s.trim().split(' ').reverse().join(' ')
+  return b
+}
+var s = "the sky is blue"
+console.log(reverseWords(s))
+
+
